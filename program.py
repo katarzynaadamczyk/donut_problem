6d72cbb61ca688bbce87caa8d711cefe3d40b120
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-10-20 22:37:26 +0200

    First attempt to connect to mySQL

--- program.py
+++ program.py
@@ -1,6 +1,23 @@
 from donut import Donut
 
+from decouple import config
+import mysql.connector
+from mysql.connector.cursor import MySQLCursor
+
+
+
 def main():
+    # Connect to MySQL database
+
+    db_mysql = mysql.connector.connect(user=config("MYSQL_DATABASE_USER"), 
+                             password=config("MYSQL_DATABASE_PASSWORD"), 
+                             host=config("MYSQL_DATABASE_HOST"), 
+                             database=config("MYSQL_DATABASE_DB"))
+
+    # create cursor to MySQL database to use stored procedures or execute queries
+    cursor = db_mysql.cursor()
+
+    
     chocolate_donut = Donut(weight=90, kcal=352, name='Chocolate donut')
     vanilla_donut = Donut(weight=80, kcal=250, name='Vanilla donut')
     print('Available donuts: ')
