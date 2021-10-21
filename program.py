e82c2c34d9e9f38c7fcd63d514404ef870b74098
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-10-21 22:36:29 +0200

    Connection to database done

--- program.py
+++ program.py
@@ -17,12 +17,13 @@ def main():
     # create cursor to MySQL database to use stored procedures or execute queries
     cursor = db_mysql.cursor()
 
+    cursor.execute("SELECT * FROM donuts")
+    portfolio = cursor.fetchall()
     
     chocolate_donut = Donut(weight=90, kcal=352, name='Chocolate donut')
     vanilla_donut = Donut(weight=80, kcal=250, name='Vanilla donut')
     print('Available donuts: ')
-    chocolate_donut.print_donut()
-    vanilla_donut.print_donut()
+    print(portfolio)
 
     
 if __name__ == '__main__':
