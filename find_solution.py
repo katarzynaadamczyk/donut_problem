a6b2cbf5c41828db710839d434f6a5e46468a26f
Katarzyna Adamczyk <kasia.cholost@gmail.com>
2021-11-23 23:06:15 +0100

    added file find_solution and started writing its class

---
+++ find_solution.py
@@ -0,0 +1,12 @@
+# TO DO 
+# implement a class to solve the problem
+
+class Find_Solution:
+    def __init__(self):
+        pass
+        
+    def find_best_option(self):
+        pass
+    
+    def backpack_alg(self):
+        pass
